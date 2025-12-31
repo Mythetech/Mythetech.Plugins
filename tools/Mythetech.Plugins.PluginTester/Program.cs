f2ae623ef6254a9f7687ef986f1923b6a83604fb
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-12-31 10:48:41 -0700

    feat: standalone plugin testing application

---
+++ tools/Mythetech.Plugins.PluginTester/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using Mythetech.Framework.Desktop;
+using Mythetech.Framework.Desktop.Photino;
+using Mythetech.Framework.Infrastructure.MessageBus;
+using Mythetech.Framework.Infrastructure.Plugins;
+using Mythetech.Plugins.Notes;
+using Mythetech.Plugins.PluginTester;
+using Photino.Blazor;
+
+var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
+
+appBuilder.Services.AddLogging();
+appBuilder.Services.AddMudServices();
+appBuilder.Services.AddMessageBus();
+appBuilder.Services.AddPhotinoServices();
+appBuilder.Services.AddLinkOpenService();
+appBuilder.Services.AddPluginStorage();
+appBuilder.Services.AddPluginFramework();
+
+appBuilder.RootComponents.Add<App>("#app");
+
+var app = appBuilder.Build();
+
+app.MainWindow.SetTitle("Plugin Tester");
+app.MainWindow.SetUseOsDefaultSize(false);
+app.MainWindow.SetSize(1920, 1080);
+app.MainWindow.SetUseOsDefaultLocation(true);
+
+app.RegisterProvider(app.Services);
+app.Services.UseMessageBus(typeof(Program).Assembly, typeof(IConsumer<>).Assembly);
+
+app.Services.UsePlugin(typeof(Manifest).Assembly);
+
+app.Run();
