dd1ddb89e65ae8c01b86020159919d62d5be2dd1
Tom Brewer <tombrewer0@gmail.com>
2026-05-01 17:36:58 -0600

    chore: update game plugin version

--- src/Mythetech.Plugins.Games/Manifest.cs
+++ src/Mythetech.Plugins.Games/Manifest.cs
@@ -7,7 +7,7 @@ public class Manifest : IPluginManifest
 {
     public string Id { get; } = ManifestConstants.Id;
     public string Name { get; } = "Games";
-    public Version Version { get; } = new Version(1, 0, 0);
+    public Version Version { get; } = new Version(1, 0, 1);
     public string Developer { get; } = "Mythetech";
     public string Description { get; } = "A collection of fun, simple single-player games";
     public string? Icon { get; } = Icons.Material.Outlined.Games;
